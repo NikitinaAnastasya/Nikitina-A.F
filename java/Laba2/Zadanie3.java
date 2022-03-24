0df7a2b43762543345fca0c6324c01a94e2512cf
Anastasya <nktnnsta@gmail.com>
2022-03-24 18:31:28 +0300

    Laba2

--- java/Laba2/Zadanie3.java
+++ java/Laba2/Zadanie3.java
@@ -1,12 +1,12 @@
 package Nikitina3;
-import java.util.Scanner;//дл€ ввода данных
+import java.util.Scanner;//dlya vvoda dannyx
 import java.util.Random;
 import java.util.ArrayList;
 public class Zadanie3 {
 	public static void main(String[] args) {
 		ArrayList <Integer> vec = new ArrayList<>();
 		Scanner in = new Scanner(System.in);
-		System.out.println("¬ведите параметры матрицы n и m соответсвенно:");
+		System.out.println("Vvedite parametry matricy n u m sootvetstveno:");
 		int n = in.nextInt();
 		int m = in.nextInt();
 		int[][] matr = new int [n][m];
